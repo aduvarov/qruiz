19bc0e71e4d1bcada3ef86f0fb80ff234de6b340
Anatoliy D. Uvarov <a.d.uvarov@gmail.com>
2023-02-02 02:24:44 +0600

    Базовая подготовка

---
+++ app.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const morgan = require('morgan');
+const chalk = require('chalk');
+const mongoose = require('mongoose');
+const createPath = require('./helpers/createPath.js');
+const express = require('express');
+const app = express();
+
+const errorMsg = chalk.bgKeyword('white').redBright;
+const successMsg = chalk.bgKeyword('green').white;
+
+app.set('view engine', 'ejs');
+
+mongoose.set('strictQuery', true);
+mongoose
+    .connect(process.env.MONGO_URL, { useNewUrlParser: true })
+    .then(res => console.log(successMsg('Connected to DB')))
+    .catch(error => console.error(errorMsg(error)));
+
+app.listen(process.env.PORT, error => {
+    error
+        ? console.log(errorMsg(error))
+        : console.log(successMsg(`Server running on port ${process.env.PORT}`));
+});
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('styles'));
